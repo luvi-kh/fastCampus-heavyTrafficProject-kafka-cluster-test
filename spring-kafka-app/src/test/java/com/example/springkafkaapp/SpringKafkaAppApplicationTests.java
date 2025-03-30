35a6a8092a6c2052469be474f026fbb4c7e24bd2
luvi-kh <nalsengom@daum.net>
2025-03-31 04:23:09 +0900

    project 올리기

---
+++ spring-kafka-app/src/test/java/com/example/springkafkaapp/SpringKafkaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springkafkaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
